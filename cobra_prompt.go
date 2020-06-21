c36c671e618a6f86f53089646316160fbbad4d6e
blukat29 <yunjong@kaist.ac.kr>
2020-06-22 03:23:49 +0900

    Command and flag autocompletion

--- cobra_prompt.go
+++ cobra_prompt.go
@@ -27,6 +27,7 @@ func New(rootCmd *cobra.Command, opts ...prompt.Option) *CobraPrompt {
 	}
 
 	opts = append(opts, prompt.OptionLivePrefix(cp.getPrefix))
+	opts = append(opts, prompt.OptionShowCompletionAtStart())
 	opts = append(opts, prompt.OptionWriter(cp.writer))
 
 	p := prompt.New(
@@ -70,12 +71,76 @@ func (cp *CobraPrompt) executor(in string) {
 // completer returns a list of suggestions that can fit in the current cursor
 // location.
 func (cp *CobraPrompt) completer(d prompt.Document) []prompt.Suggest {
-	return nil
+	items := []prompt.Suggest{}
+	words, last, curr := splitDocument(d)
+	_ = last
+
+	// 1. Find current command
+	cmd := cp.RootCmd
+	found, _, err := cmd.Find(words)
+	if err == nil {
+		cmd = found
+	}
+
+	// 2. Add subcommands to suggestions
+	commandItems := suggestCommand(cmd, words, curr)
+	items = append(items, commandItems...)
+
+	// 3. Add flags to suggestions
+	flagItems := suggestFlags(cmd, words, curr)
+	items = append(items, flagItems...)
+
+	return items
 }
 
 // getPrefix returns the prompt string (something like "shell> ").
 func (cp *CobraPrompt) getPrefix() (prefix string, useLivePrefix bool) {
 	return cp.promptPrefix, true
+}
+
+func suggestCommand(cmd *cobra.Command, words []string, curr string) []prompt.Suggest {
+	items := []prompt.Suggest{}
+	for _, c := range cmd.Commands() {
+		if !c.IsAvailableCommand() {
+			continue
+		}
+		if prefixMatches(c.Name(), curr) {
+			items = append(items, prompt.Suggest{
+				Text:        c.Name(),
+				Description: c.Short})
+		}
+	}
+	return items
+}
+
+func suggestFlags(cmd *cobra.Command, words []string, curr string) []prompt.Suggest {
+	items := []prompt.Suggest{}
+
+	visit := func(f *pflag.Flag) {
+		if f.Hidden {
+			return
+		}
+
+		name := "--" + f.Name
+		if prefixMatches(name, curr) {
+			items = append(items, prompt.Suggest{
+				Text:        name,
+				Description: f.Usage})
+		}
+
+		if f.Shorthand != "" {
+			shorthand := "-" + f.Shorthand
+			if prefixMatches(shorthand, curr) {
+				items = append(items, prompt.Suggest{
+					Text:        shorthand,
+					Description: f.Usage})
+			}
+		}
+	}
+
+	cmd.LocalFlags().VisitAll(visit)
+	cmd.InheritedFlags().VisitAll(visit)
+	return items
 }
 
 func splitString(in string) []string {
@@ -87,6 +152,35 @@ func splitString(in string) []string {
 		words = strings.Fields(in)
 	}
 	return words
+}
+
+func splitDocument(d prompt.Document) (words []string, last string, curr string) {
+	words = splitString(d.Text)
+
+	if d.GetWordBeforeCursor() == "" {
+		// 1) cursor is at whitespace.
+		//        [info --name ]
+		//                    ^
+		if len(words) >= 1 {
+			last = words[len(words)-1]
+		}
+		curr = ""
+	} else {
+		// 2) cursor is in the middle of a word.
+		//        [info --name abc]
+		//                       ^
+		if len(words) >= 2 {
+			last = words[len(words)-2]
+		}
+		if len(words) >= 1 {
+			curr = words[len(words)-1]
+		}
+	}
+	return
+}
+
+func prefixMatches(s, prefix string) bool {
+	return strings.HasPrefix(s, prefix)
 }
 
 // resetFlagValues resets values of all flags in a command to their default
